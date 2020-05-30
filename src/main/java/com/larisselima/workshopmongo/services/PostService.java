c8f7635e9e71ff7175713cd22fa611e3c2d743b3
Larisse Lima <larisse.lima2@gmail.com>
2020-05-30 16:27:49 -0300

    Obtendo um post por id

---
+++ src/main/java/com/larisselima/workshopmongo/services/PostService.java
@@ -0,0 +1,25 @@
+package com.larisselima.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.larisselima.workshopmongo.domain.Post;
+import com.larisselima.workshopmongo.repository.PostRepository;
+import com.larisselima.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	
+}
+
